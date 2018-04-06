ecd90090ae8cb31ca38622c3b0f55606a3ff6e5c
DVucetaj <Dvucetaj@gmail.com>
2018-04-06 14:40:59 -0400

    I'm gonna fix this i swear

--- server/server.js
+++ server/server.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
 const PORT = process.env.PORT || 8000;
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
   const app = express();
 
   // Priority serve any static files.
@@ -35,7 +19,4 @@ if (cluster.isMaster) {
     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-  });
-}
+  app.listen(PORT);

a153e10b4818593d756a96cee850a5992fb14c8e
DVucetaj <Dvucetaj@gmail.com>
2018-04-06 14:42:26 -0400

    I'm gonna fix this i swear pt2

--- server/server.js
+++ server/server.js
@@ -8,12 +8,6 @@ const PORT = process.env.PORT || 8000;
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));

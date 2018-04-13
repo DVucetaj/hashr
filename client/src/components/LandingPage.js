400e608767b459636cb5181674888c2d7daca6b4
Zishan Ahmed <ZishanAhmed93@gmail.com>
2018-04-13 13:03:56 -0400

    added landingpage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,17 +4,52 @@ import SearchBar from './SearchBar.js';
 import './LandingPage.css';
 
 class LandingPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      vW: window.innerWidth,
+      vH: window.innerHeight,
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({vW: window.innerWidth});
+    this.setState({vH: window.innerHeight});
+  }
+
   render() {
+      
+      let vW = this.state.vW;
+      let vH = this.state.vH;
+
+      {console.log(vW)}
+      {console.log(vH)}
+    
+
       return (
+
+
       <div>
 
-        <svg className="lpBackground" height="1024" width="1440">
-          <polygon className="lpRed" points="0,0 1082,0 812,754 0,754 "/>
-          <polygon className="lpBlue" points="572,0 1440,0 1440,754 300,754 "/>
-          <polygon className="lpGreen" points="486,238 1440,238 1440,1024 203,1024 "/>
-          <polygon className="lpYellow" points="0,238 997,238 717,1024 0,1024 "/>
-          <polygon className="lpWhite" points="838,356 566,356 462,636 738,636 "/>
-          
+        <svg className="lpBackground" width="1440" height={vH}>
+         
+          <polygon className="lpRed"    points={`${vW*(0/1440)},${vH*(0/1024)} ${vW*(1082/1440)},${vH*(0/1024)} ${vW*(812/1440)},${vH*(754/1024)} ${vW*(0/1440)},${vH*(754/1024)} `} />
+          <polygon className="lpBlue"   points={`${vW*(572/1440)},${vH*(0/1024)} ${vW*(300/1440)},${vH*(754/1024)} ${vW*(1440/1440)},${vH*(754/1024)} ${vW*(1440/1440)},${vH*(0/1024)}`}/>    
+          <polygon className="lpGreen"  points={`${vW*(0/1440)},${vH*(238/1024)} ${vW*(0/1440)},${vH*(1024/1024)} ${vW*(717/1440)},${vH*(1024/1024)} ${vW*(997/1440)},${vH*(238/1024)}`}/>
+          <polygon className="lpYellow" points={`${vW*(486/1440)},${vH*(238/1024)} ${vW*(203/1440)},${vH*(1024/1024)}  ${vW*(1440/1440)},${vH*(1024/1024)} ${vW*(1440/1440)},${vH*(238/1024)}`}/>
+          <polygon className="lpWhite"  points={`${vW*(566/1440)},${vH*(356/1024)} ${vW*(838/1440)},${vH*(356/1024)}  ${vW*(738/1440)},${vH*(636/1024)} ${vW*(462/1440)},${vH*(636/1024)}`}/>
+      
+
         </svg>
 
 
